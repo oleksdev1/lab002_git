4e796ae28419c394f0999e314bf5407bf54625b2
nio <bhf@gjhgj.com>
2021-08-29 15:10:41 +0300

    Add new strings.py file Add function mul() correct syntax delete text variable

--- first/nums.py
+++ first/nums.py
@@ -1,12 +1,14 @@
 def plus(a, b):
     return a + b
 
-text = "new text"
-
-def mimus(a, b):
+def minus(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
+
 if __name__ == '__main__':
-    print(plus(2, 2))
+    print( plus(2, 2))
     print( minus(5, 2))
+    print( mul(3, 3))
 
